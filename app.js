d84afbbeb717d7d36155c106308bf6f135351c6e
Beau Vandervort <beauvandervort55@gmail.com>
2022-11-09 17:37:47 -0800

    create displayBeanies(), fetch-utils functions, render-utils function

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 /* Imports */
-
+import { getAstroSigns, getBeanies } from './fetch-utils.js';
+import { renderAstroSign, renderBeanie } from './render-utils.js';
 /* Get DOM Elements */
 const beanieList = document.getElementById('beanie-list');
 const astroSignSelect = document.getElementById('astro-sign-select');
@@ -10,7 +11,20 @@ let beanies = [];
 let astroSigns = [];
 
 /* Events */
+async function findBeanies(astroSign) {
+    const response = await getBeanies(astroSign);
+    beanies = response.data;
+    displayBeanies();
+}
 
 /* Display Functions */
+function displayBeanies() {
+    beanieList.innerHTML = '';
+
+    for (let beanie of beanies) {
+        const beanieEl = renderBeanie(beanie);
+        beanieList.append(beanieEl);
+    }
+}
 
 // (don't forget to call any display functions you want to run on page load!)
